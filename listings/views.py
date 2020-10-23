0435b6a52004bb6397b8944ecb8fbe425d25aa9c
blagovest <bfartsov@htv.bg>
2020-10-23 17:39:44 +0300

    started to implement search

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 
@@ -28,4 +29,12 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+
+    }
+
+    return render(request, 'listings/search.html', context)
